8f4450b5274d38169db76c60482a750ecbe9af2c
Zacharie Montreuil <zacharie.montreuil@mitt.ca>
2023-06-12 09:57:26 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// abstract class, cannot be instantiated
+abstract class Animal
+{
+    // members: fields and properties
+    // field
+    private int _limbs;
+    private string _name;
+
+    // methods
+    public void SayName()
+    {
+        Console.WriteLine($"Hello, my name is {_name}");
+    }
+}
+
