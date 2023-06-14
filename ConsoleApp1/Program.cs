3a6bb96b37c4580d22ae4c95d899f3baae72d2e5
Zacharie Montreuil <zacharie.montreuil@mitt.ca>
2023-06-14 11:20:31 -0500

    Refactor school to fulfill 13 June requirements

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,56 @@
-﻿string sName = "Bob";
-string cName = "Course";
+﻿try
+{
+    School.RegisterStudent("Bob", "Turkey", "bob@school.ca");
+    School.RegisterInstructor("Linda", "Canada", "linda@school.ca", 10000000);
 
-School.RegisterStudent(sName);
-School.RegisterStudent("Other Person");
-School.RegisterStudent("Name Namerson");
 
-School.RegisterCourse(cName);
-School.RegisterCourse("Advanced Courses");
+    School.RegisterCourse("Intro to Burgers");
+    School.RegisterCourse("Advanced Courses");
 
-Student studentToAdd = School.GetStudent(sName);
-Course courseToAdd = School.GetCourse(cName);
+    Student studentToAdd = School.GetStudent("Bob");
+    Course courseToAdd = School.GetCourse("Intro to Burgers");
 
-School.RegisterStudentForCourse(studentToAdd, courseToAdd);
+    School.RegisterPersonForCourse(studentToAdd, courseToAdd);
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 static class School
 {
     public static string Name { get; } = "MITT";
+    
     private static HashSet<Course> _courses = new HashSet<Course>();
     private static HashSet<Student> _students = new HashSet<Student>();
+    private static HashSet<Instructor> _instructors = new HashSet<Instructor>();
 
-    public static void RegisterStudent(string newStudentName)
+    private static int _idCount = 1;
+
+    public static void RegisterStudent(string newStudentName, 
+        string newStudentCountry, 
+        string newStudentEmail)
     {
-        Student newStudent = new Student(newStudentName);
+        Student newStudent = new Student(_idCount, 
+            newStudentName, 
+            newStudentCountry, 
+            newStudentEmail);
+
+        _idCount++;
         _students.Add(newStudent);
+    }
+    public static void RegisterInstructor(string newInstructorName, 
+        string newInstructorCountry, 
+        string newInstructorEmail, 
+        int newInstructorSalary)
+    {
+        Instructor newInstructor = new Instructor(_idCount, 
+            newInstructorName, 
+            newInstructorCountry, 
+            newInstructorEmail, 
+            newInstructorSalary);
+
+        _idCount++;
+        _instructors.Add(newInstructor);
     }
 
     public static void RegisterCourse(string courseTitle)
@@ -31,11 +59,24 @@ static class School
         _courses.Add(newCourse);
         Console.WriteLine($"Total of {_courses.Count} courses.");
     }
-
-    public static void RegisterStudentForCourse(Student student, Course course)
+    public static void RegisterPersonForCourse(Person person, Course course)
     {
-        student.SetCourse(course);
-        course.AddStudentToCourse(student);
+        person.SetCourse(course);
+
+        if(person is Instructor)
+        {
+            course.SetInstructor((Instructor)person);
+        } else if (person is Student)
+        {
+            if(course.GetStudentCount() < course.Capacity)
+            {
+                course.AddStudentToCourse((Student)person);
+            }
+            else
+            {
+                throw new InvalidOperationException("Student count cannot exceed course capacity.");
+            }
+        }
     }
 
     public static Student? GetStudent(string studentName)
@@ -43,37 +84,24 @@ static class School
         Student foundStudent = _students.First(s => s.FullName == studentName);
         return foundStudent;
     }
-
     public static Course? GetCourse(string courseName)
     {
         Course foundCourse = _courses.First(c => c.Title == courseName);
         return foundCourse;
     }
-
+    public static Instructor? GetInstructor(int? id)
+    {
+        if (id == null)
+        {
+            throw new ArgumentNullException("This method requires an Id value that is not null");
+        }
+        Instructor foundInstructor = _instructors.First(i => i.RegistrationNumber == id);
+        return foundInstructor;
+    }
 
     static School()
     {
 
-    }
-}
-
-class Student
-{
-    private string _fullName;
-    public string FullName { get {  return _fullName; } }
-
-    private Course _course;
-    public void SetCourse(Course course)
-    {
-        _course = course;
-    }
-
-    public Student(string fullName)
-    {
-        if (!String.IsNullOrEmpty(fullName))
-        {
-            _fullName = fullName;
-        }
     }
 }
 
@@ -82,21 +110,141 @@ class Course
     private string _title;
     public string Title { get { return _title; } }
 
+    private int _capacity;
+    public int Capacity { get { return _capacity; } }
+
+
+    private Instructor _instructor;
     private HashSet<Student> _students = new HashSet<Student>();
+    public double GetAverageGrade()
+    {
+        double totalGrade = 0;
+
+        foreach(Student s in _students)
+        {
+            totalGrade += s.Grade;
+        }
+
+        double average = totalGrade / _students.Count;
+        return average;
+    }
     public void AddStudentToCourse(Student student)
     {
         _students.Add(student);
     }
-
-    public Course(string title)
+    public void SetInstructor(Instructor instructor)
+    {
+        _instructor = instructor;
+    }
+    public void SetCapacity(int capacity)
+    {
+        if (capacity <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Capacity must be greater than 0");
+        } else
+        {
+            _capacity = capacity;
+        }
+    }
+    public int GetStudentCount()
+    {
+        return _students.Count;
+    }
+    public Course(string title, int capacity)
     {
         if (!String.IsNullOrEmpty(title))
         {
             _title = title;
+            SetCapacity(capacity);
+        } else
+        {
+            throw new ArgumentNullException("Course title must not be empty");
         }
+
     }
 }
-// Students can Enroll in one course
-// Courses can have many students in them
-// These belong to a static School class
-// School has an AddStudentToCourse(Student s, Course c) method defined on it
+
+
+abstract class Person 
+{
+    private int _registrationNumber;
+    public int RegistrationNumber { get { return _registrationNumber; } }
+
+    private string _fullName;
+    public string FullName { get { return _fullName;} }
+
+    private string _countryOfOrigin;
+    public string CountryOfOrigin { get { return _countryOfOrigin;} }
+
+    private string _contactEmail;
+    public string ContactEmail { get { return _contactEmail;} }
+    public abstract void SetCourse(Course course);
+
+    public Person(int regNumber, string name, string country, string email)
+    {
+        // todo: validate all arguments
+        _registrationNumber = regNumber;
+        _fullName = name;
+        _countryOfOrigin = country;
+        _contactEmail = email;
+    }
+
+}
+
+// Student and Instructor
+class Student: Person
+{
+    private int _grade;
+    public int Grade
+    {
+        get { return _grade; }
+        set
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentOutOfRangeException("Grades must fall within range of 0 to 100");
+            } else
+            {
+                _grade = value;
+            }
+        }
+    }
+
+    private Course _enrolledCourse;
+
+    // abstract methods MUST be overridden (given their own body) in a child class
+    public override void SetCourse(Course course)
+    {
+        _enrolledCourse = course;
+    }
+    // Student s = new Student(1, "stude", "here", "stude@school.ca");
+    public Student(int regNumber, string name, string country, string email): base(regNumber, name, country, email)
+    {
+
+    }
+
+    public Student(int regNumber, string name, string country, string email, Course course) : base(regNumber, name, country, email)
+    {
+        SetCourse(course);
+    }
+}
+class Instructor: Person
+{
+    private Course _instructedCourse;
+    private int _salary;
+    public override void SetCourse(Course course)
+    {
+        _instructedCourse = course;
+    }
+
+    public Instructor(int regNumber, string name, string country, string email, int salary) : base(regNumber, name, country, email)
+    {
+        _salary = salary;
+    }
+
+    public Instructor(int regNumber, string name, string country, string email, int salary, Course course) : base(regNumber, name, country, email)
+    {
+        _salary = salary;
+        SetCourse(course);
+    }
+}
